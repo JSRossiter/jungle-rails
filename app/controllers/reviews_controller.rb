0912cc19eb293d1809c5ab65b41737e51287b587
JSRossiter <jeffrey.rossiter@me.com>
2017-05-11 13:25:23 -0700

    users can post and delete reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,38 @@
+class ReviewsController < ApplicationController
+  before_filter do
+    redirect_to :root unless current_user
+  end
+
+  def create
+    @product = Product.find params[:product_id]
+    @review = @product.reviews.build(review_params)
+    @review.user = current_user
+
+    if @review.save
+      redirect_to @product
+    else
+      render 'products/show'
+    end
+  end
+
+  def destroy
+    review = Review.find params[:id]
+    @product = Product.find params[:product_id]
+    if current_user = review.user
+      review.destroy
+      redirect_to @product
+    else
+      redirect_to @product
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :description,
+      :rating
+    )
+  end
+
+end
