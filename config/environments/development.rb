fd29b3d28d63ab2acbf1e69fcce4ceaec5babd20
JSRossiter <jeffrey.rossiter@me.com>
2017-05-11 16:31:40 -0700

    disable mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   config.web_console.whitelisted_ips = ['10.0.2.0/24']
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
