f495ed15dbe2427ed67a52f50501ae294a18acc0
JSRossiter <jeffrey.rossiter@me.com>
2017-05-11 14:10:10 -0700

    display errors when rating isn't selected

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,7 +4,7 @@ class Review < ActiveRecord::Base
   belongs_to :product
 
   validates :rating, presence: true
-  validates :rating, inclusion: { in: 1..5 }
+  validates :rating, inclusion: { in: 1..5, message: "must be a number between 1 and 5" }
   validates :user, presence: true
   validates :product, presence: true
 
