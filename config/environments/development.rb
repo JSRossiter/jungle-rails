11b5665d2bb08e053643a46f0be6262f7af50065
JSRossiter <jeffrey.rossiter@me.com>
2017-05-11 09:54:29 -0700

    emails send

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,16 +41,17 @@ Rails.application.configure do
 
   config.web_console.whitelisted_ips = ['10.0.2.0/24']
 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.perform_deliveries = true
-
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'jungle.com',
+    domain:               'gmail.com',
     user_name:            ENV['GMAIL_USERNAME'],
-    password:             ENV['GMAIL_USERNAME'],
-    authentication:       'plain',
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       :plain,
     enable_starttls_auto: true
   }
 
