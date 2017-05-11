008e906d48fc83f78287ccd69e660a48e4117e09
JSRossiter <jeffrey.rossiter@me.com>
2017-05-11 14:01:36 -0700

    handle blank review posting

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,10 +8,11 @@ class ReviewsController < ApplicationController
     @review = @product.reviews.build(review_params)
     @review.user = current_user
 
+
     if @review.save
       redirect_to @product
     else
-      render 'products/show'
+      render :'products/show'
     end
   end
 
