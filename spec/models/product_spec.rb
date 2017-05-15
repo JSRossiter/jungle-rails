0ac11e5c90234265e7105df91d6f6d34ff2a1f78
JSRossiter <jeffrey.rossiter@me.com>
2017-05-15 13:55:44 -0700

    updated tests to only use database when required

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,37 +3,32 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     it 'should create a new product with valid inputs' do
-      @category = Category.new(name: 'a')
-      @product = @category.products.new(name: 'Bike', price: 100, quantity: 10)
-      @product.save
-      expect(Product.count).to eql(1)
+      category = Category.new(name: 'a')
+      product = category.products.new(name: 'Bike', price: 100, quantity: 10)
+      expect(product).to be_valid
     end
     it 'should return an error when missing name' do
-      @category = Category.new(name: 'a')
-      @product = @category.products.new(price: 100, quantity: 10)
-      @product.save
-      expect(Product.count).to eql(0)
-      expect(@product.errors.full_messages).to include("Name can't be blank")
+      category = Category.new(name: 'a')
+      product = category.products.new(price: 100, quantity: 10)
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Name can't be blank")
     end
     it 'should return an error when missing price' do
-      @category = Category.new(name: 'a')
-      @product = @category.products.new(name: 'Bike', quantity: 10)
-      @product.save
-      expect(Product.count).to eql(0)
-      expect(@product.errors.full_messages).to include("Price can't be blank")
+      category = Category.new(name: 'a')
+      product = category.products.new(name: 'Bike', quantity: 10)
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Price can't be blank")
     end
     it 'should return an error when missing quantity' do
-      @category = Category.new(name: 'a')
-      @product = @category.products.new(name: 'Bike', price: 100)
-      @product.save
-      expect(Product.count).to eql(0)
-      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+      category = Category.new(name: 'a')
+      product = category.products.new(name: 'Bike', price: 100)
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Quantity can't be blank")
     end
     it 'should return an error when missing category' do
-      @product = Product.new(name: 'Bike', price: 100, quantity: 10)
-      @product.save
-      expect(Product.count).to eql(0)
-      expect(@product.errors.full_messages).to include("Category can't be blank")
+      product = Product.new(name: 'Bike', price: 100, quantity: 10)
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Category can't be blank")
     end
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,51 +4,42 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
     it 'should return an error when password_confirmation is omitted' do
       user = User.new(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password')
-      user.save
-      expect(User.count).to eql(0)
+      expect(user).to_not be_valid
       expect(user.errors.full_messages).to include("Password confirmation can't be blank")
     end
     it 'should require password and password_confirmation to match' do
       user = User.new(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'not_test_password')
-      user.save
-      expect(User.count).to eql(0)
+      expect(user).to_not be_valid
       expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
     it 'should create a user with valid inputs' do
       user = User.new(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
-      user.save
-      expect(User.count).to eql(1)
+      expect(user).to be_valid
     end
     it 'should require emails to be unique, case insensitive' do
-      user_a = User.new(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
-      user_a.save
+      user_a = User.create(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
       user_b = User.new(first_name: 'b', last_name: 'b', email: '1@1.COM', password: 'test_password', password_confirmation: 'test_password')
-      user_b.save
-      expect(User.count).to eql(1)
+      expect(user_b).to_not be_valid
       expect(user_b.errors.full_messages).to include("Email has already been taken")
     end
     it 'should return an error when missing email' do
       user = User.new(first_name: 'a', last_name: 'a', password: 'test_password', password_confirmation: 'test_password')
-      user.save
-      expect(User.count).to eql(0)
+      expect(user).to_not be_valid
       expect(user.errors.full_messages).to include("Email can't be blank")
     end
     it 'should return an error when missing first_name' do
       user = User.new(last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
-      user.save
-      expect(User.count).to eql(0)
+      expect(user).to_not be_valid
       expect(user.errors.full_messages).to include("First name can't be blank")
     end
     it 'should return an error when missing last_name' do
       user = User.new(first_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
-      user.save
-      expect(User.count).to eql(0)
+      expect(user).to_not be_valid
       expect(user.errors.full_messages).to include("Last name can't be blank")
     end
     it 'should return an error when password is too short' do
       user = User.new(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'five5', password_confirmation: 'five5')
-      user.save
-      expect(User.count).to eql(0)
+      expect(user).to_not be_valid
       expect(user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
     end
   end
