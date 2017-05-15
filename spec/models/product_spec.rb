67b89a0c3f7678e3f92d625b7d1ab18a19eb3459
JSRossiter <jeffrey.rossiter@me.com>
2017-05-15 14:31:34 -0700

    refactor using before method

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,33 +2,33 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    it 'should create a new product with valid inputs' do
+    before :each do
       category = Category.new(name: 'a')
-      product = category.products.new(name: 'Bike', price: 100, quantity: 10)
-      expect(product).to be_valid
+      @product = category.products.new(name: 'Bike', price: 100, quantity: 10)
+    end
+
+    it 'should create a new product with valid inputs' do
+      expect(@product).to be_valid
     end
     it 'should return an error when missing name' do
-      category = Category.new(name: 'a')
-      product = category.products.new(price: 100, quantity: 10)
-      expect(product).to_not be_valid
-      expect(product.errors.full_messages).to include("Name can't be blank")
+      @product.name = nil
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
     it 'should return an error when missing price' do
-      category = Category.new(name: 'a')
-      product = category.products.new(name: 'Bike', quantity: 10)
-      expect(product).to_not be_valid
-      expect(product.errors.full_messages).to include("Price can't be blank")
+      @product.price_cents = nil
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Price can't be blank")
     end
     it 'should return an error when missing quantity' do
-      category = Category.new(name: 'a')
-      product = category.products.new(name: 'Bike', price: 100)
-      expect(product).to_not be_valid
-      expect(product.errors.full_messages).to include("Quantity can't be blank")
+      @product.quantity = nil
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
     it 'should return an error when missing category' do
-      product = Product.new(name: 'Bike', price: 100, quantity: 10)
-      expect(product).to_not be_valid
-      expect(product.errors.full_messages).to include("Category can't be blank")
+      @product.category = nil
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,76 +2,78 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
+    before :each do
+      @user = User.new(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
+    end
+
     it 'should return an error when password_confirmation is omitted' do
-      user = User.new(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password')
-      expect(user).to_not be_valid
-      expect(user.errors.full_messages).to include("Password confirmation can't be blank")
+      @user.password_confirmation = nil
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Password confirmation can't be blank")
     end
     it 'should require password and password_confirmation to match' do
-      user = User.new(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'not_test_password')
-      expect(user).to_not be_valid
-      expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      @user.password_confirmation = 'not_test_password'
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
     it 'should create a user with valid inputs' do
-      user = User.new(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
-      expect(user).to be_valid
+      expect(@user).to be_valid
     end
     it 'should require emails to be unique, case insensitive' do
-      user_a = User.create(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
+      @user.save
       user_b = User.new(first_name: 'b', last_name: 'b', email: '1@1.COM', password: 'test_password', password_confirmation: 'test_password')
       expect(user_b).to_not be_valid
       expect(user_b.errors.full_messages).to include("Email has already been taken")
     end
     it 'should return an error when missing email' do
-      user = User.new(first_name: 'a', last_name: 'a', password: 'test_password', password_confirmation: 'test_password')
-      expect(user).to_not be_valid
-      expect(user.errors.full_messages).to include("Email can't be blank")
+      @user.email = nil
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Email can't be blank")
     end
     it 'should return an error when missing first_name' do
-      user = User.new(last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
-      expect(user).to_not be_valid
-      expect(user.errors.full_messages).to include("First name can't be blank")
+      @user.first_name = nil
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("First name can't be blank")
     end
     it 'should return an error when missing last_name' do
-      user = User.new(first_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
-      expect(user).to_not be_valid
-      expect(user.errors.full_messages).to include("Last name can't be blank")
+      @user.last_name = nil
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
     end
     it 'should return an error when password is too short' do
-      user = User.new(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'five5', password_confirmation: 'five5')
-      expect(user).to_not be_valid
-      expect(user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      @user.password = 'five5'
+      @user.password_confirmation = 'five5'
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
     end
   end
 
   describe '.authenticate_with_credentials' do
-    it 'should return user when provided correct credentials' do
+    before :each do
       User.create(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
+    end
+
+    it 'should return user when provided correct credentials' do
       user = User.authenticate_with_credentials('1@1.com', 'test_password')
       expect(user).to eql(User.find_by_email('1@1.com'))
     end
     it 'should return false when provided incorrect password' do
-      User.create(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
       user = User.authenticate_with_credentials('1@1.com', 'not_test_password')
       expect(user).to eql(false)
     end
     it 'should return false when provided incorrect email' do
-      User.create(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
       user = User.authenticate_with_credentials('not@email.com', 'test_password')
       expect(user).to eql(false)
     end
     it 'should return user when provided email with different case characters' do
-      User.create(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
       user = User.authenticate_with_credentials('1@1.COM', 'test_password')
       expect(user).to eql(User.find_by_email('1@1.com'))
     end
     it 'should return user when provided email with white space after' do
-      User.create(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
       user = User.authenticate_with_credentials('1@1.com ', 'test_password')
       expect(user).to eql(User.find_by_email('1@1.com'))
     end
     it 'should return user when provided with white space before' do
-      User.create(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
       user = User.authenticate_with_credentials(' 1@1.com', 'test_password')
       expect(user).to eql(User.find_by_email('1@1.com'))
     end
