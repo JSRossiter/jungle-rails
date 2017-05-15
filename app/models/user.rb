8bbe67c1fb49bf2a267f79f6ea514258704e3dd0
JSRossiter <jeffrey.rossiter@me.com>
2017-05-15 12:46:36 -0700

    completed tests for authenticate_with_credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,15 @@ class User < ActiveRecord::Base
 
   has_many :reviews
 
-  validates :first_name, :email, presence: true
-  validates :email, uniqueness: true
+  validates :first_name, :last_name, :email, :password_confirmation, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
+
+  def self.authenticate_with_credentials (email, password)
+    user = User.where("lower(email) = ?", email.downcase.strip).first
+    return false unless user
+    user.authenticate(password)
+  end
+
 
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,89 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    it 'should return an error when password_confirmation is omitted' do
+      user = User.new(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password')
+      user.save
+      expect(User.count).to eql(0)
+      expect(user.errors.full_messages).to include("Password confirmation can't be blank")
+    end
+    it 'should require password and password_confirmation to match' do
+      user = User.new(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'not_test_password')
+      user.save
+      expect(User.count).to eql(0)
+      expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+    it 'should create a user with valid inputs' do
+      user = User.new(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
+      user.save
+      expect(User.count).to eql(1)
+    end
+    it 'should require emails to be unique, case insensitive' do
+      user_a = User.new(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
+      user_a.save
+      user_b = User.new(first_name: 'b', last_name: 'b', email: '1@1.COM', password: 'test_password', password_confirmation: 'test_password')
+      user_b.save
+      expect(User.count).to eql(1)
+      expect(user_b.errors.full_messages).to include("Email has already been taken")
+    end
+    it 'should return an error when missing email' do
+      user = User.new(first_name: 'a', last_name: 'a', password: 'test_password', password_confirmation: 'test_password')
+      user.save
+      expect(User.count).to eql(0)
+      expect(user.errors.full_messages).to include("Email can't be blank")
+    end
+    it 'should return an error when missing first_name' do
+      user = User.new(last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
+      user.save
+      expect(User.count).to eql(0)
+      expect(user.errors.full_messages).to include("First name can't be blank")
+    end
+    it 'should return an error when missing last_name' do
+      user = User.new(first_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
+      user.save
+      expect(User.count).to eql(0)
+      expect(user.errors.full_messages).to include("Last name can't be blank")
+    end
+    it 'should return an error when password is too short' do
+      user = User.new(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'five5', password_confirmation: 'five5')
+      user.save
+      expect(User.count).to eql(0)
+      expect(user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+    it 'should return user when provided correct credentials' do
+      User.create(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
+      user = User.authenticate_with_credentials('1@1.com', 'test_password')
+      expect(user).to eql(User.find_by_email('1@1.com'))
+    end
+    it 'should return false when provided incorrect password' do
+      User.create(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
+      user = User.authenticate_with_credentials('1@1.com', 'not_test_password')
+      expect(user).to eql(false)
+    end
+    it 'should return false when provided incorrect email' do
+      User.create(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
+      user = User.authenticate_with_credentials('not@email.com', 'test_password')
+      expect(user).to eql(false)
+    end
+    it 'should return user when provided email with different case characters' do
+      User.create(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
+      user = User.authenticate_with_credentials('1@1.COM', 'test_password')
+      expect(user).to eql(User.find_by_email('1@1.com'))
+    end
+    it 'should return user when provided email with white space after' do
+      User.create(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
+      user = User.authenticate_with_credentials('1@1.com ', 'test_password')
+      expect(user).to eql(User.find_by_email('1@1.com'))
+    end
+    it 'should return user when provided with white space before' do
+      User.create(first_name: 'a', last_name: 'a', email: '1@1.com', password: 'test_password', password_confirmation: 'test_password')
+      user = User.authenticate_with_credentials(' 1@1.com', 'test_password')
+      expect(user).to eql(User.find_by_email('1@1.com'))
+    end
+  end
+
+end
